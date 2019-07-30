d7c45d41f08a82b21aeae4217e67ad53ad629c76
傅诚 <327619852@qq.com>
2019-07-30 15:30:41 +0800

    feat: 拆分store

--- client/store/sotre.js
+++ client/store/sotre.js
@@ -1,17 +1,9 @@
-
 import Vue from 'vue';
-
-
+import defaultState from './state/state';
+import mutations from './mutations/mutations';
 const store = new Vuex.Store({
-  state: {
-    count: 0
-  },
-  mutations: {  // 通过mutations 修改store里的数据
-    updateCount (state, num) {
-      state.count = num  //更新count
-      // console.log(state.count)
-    }
-  }
+  state: defaultState,
+  mutations
 })
 
 export default store
